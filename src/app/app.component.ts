8912f3d4e997e2620b389a678b7efb3999214f24
JhonnysDS <98484070+JhonnysDS@users.noreply.github.com>
2022-11-23 17:00:01 -0500

    Descargar y subir archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { UsersService } from './services/users.service';
-
+import { FilesService } from './services/files.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,9 +10,11 @@ export class AppComponent {
   imgParent = '';
   showImg = true;
   token = '';
+  imgRta = '';
 
   constructor(
-    private usersService: UsersService
+    private usersService: UsersService,
+    private filesService: FilesService
   ){}
 
   onLoaded(img: string) {
@@ -33,5 +35,22 @@ export class AppComponent {
     .subscribe(rta=>{
       console.log(rta)
     })
+  }
+  downloadPdf(){
+    this.filesService.getFile('myPDF', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf' )
+    .subscribe()
+  }
+
+  onUpload(event: Event){
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0);
+    if (file){
+      this.filesService.uploadFiles(file)
+      .subscribe(rta => {
+        this.imgRta = rta.location;
+  
+      })
+    }
+
   }
 }
--- src/app/services/files.service.ts
+++ src/app/services/files.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/Http';
+
+import { map, tap } from 'rxjs/operators';
+import {saveAs} from 'file-saver';
+import { environment } from './../../environments/environment';
+
+interface File{
+  originalname: string,
+  filename: string,
+  location: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilesService {
 
-  constructor() { }
+  private apiUrl = `${environment.API_URL}/api/files`;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+ getFile(name: string, url: string, type: string){
+    return this.http.get(url, {responseType: 'blob'})
+    .pipe(
+      tap(content => {
+        const blob = new Blob([content], {type});
+        saveAs(blob, name)
+      }),
+      map(()=> true)
+    )
+  }
+
+  uploadFiles(file: Blob){
+    const dto = new FormData();
+    dto.append('file', file)
+    return this.http.post<File>(`${this.apiUrl}/upload`, dto, 
+    // {
+    // headers:{
+    //   'content-type': "multipart/form-data"
+    // }}
+    )
+  }
+
 }
