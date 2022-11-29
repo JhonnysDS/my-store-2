5ce4d2d6427bb497713193dd810d6d32b74cee62
JhonnysDS <98484070+JhonnysDS@users.noreply.github.com>
2022-11-29 17:15:16 -0500

    Creando website module minuto 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,69 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './website/pages/home/home.component';
+
 import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { MyCartComponent } from './website/pages/my-cart/my-cart.component';
-import { LoginComponent } from './website/pages/login/login.component';
-import { RegisterComponent } from './website/pages/register/register.component';
-import { RecoveryComponent } from './website/pages/recovery/recovery.component';
-import { ProfileComponent } from './website/pages/profile/profile.component';
-import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
-import { LayoutComponent } from './website/components/layout/layout.component';
+
+
 const routes: Routes = [
   // {
   //   path: '',
   //   redirectTo: 'home',
   //   pathMatch: 'full'
   // },
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-      },
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'category/:id',
-        component: CategoryComponent
-      },
-      {
-        path: 'product/:id',
-        component: ProductDetailComponent
-      },
-      {
-        path: 'notFound',
-        component: NotFoundComponent
-      },
-      {
-        path: 'myCart',
-        component: MyCartComponent
-      },
-      {
-        path: 'login',
-        component: LoginComponent
-      },
-      {
-        path: 'register',
-        component: RegisterComponent
-      },
-      {
-        path: 'recovery',
-        component: RecoveryComponent
-      },
-      {
-        path: 'profile',
-        component: ProfileComponent
-      },
-      
-    ]
-  },
   {
     path: 'cms',
     loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule)
