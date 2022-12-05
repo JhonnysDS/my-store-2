2177d5b4890284360fbd00d0e7dbbeb665cc7ab4
JhonnysDS <98484070+JhonnysDS@users.noreply.github.com>
2022-12-05 15:14:33 -0500

    Manegando guard deactivate (para pedir confirmación para salir)

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { onExit } from 'src/app/guards/exit.guard';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, onExit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  //Importamos el guardian, y la interfaz del exit para manejor el guardian con logica
+  //desde este componente
+  onExit(){
+    const rta = confirm('Logic from component, are you sure to leave this site?');
+    return rta;
+  }
 }
